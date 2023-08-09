1b665afbd5c1c244e3a4608dd4bd855cec306b2a
Lua Lemes <lualemes@gmail.com>
2023-08-09 14:06:01 -0300

    Arreglando error

--- evento.js
+++ evento.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded",function(){
 
-    let contenedor = document.addEventListener("click", function(){
+    let contenedor = document.getElementById("divClick")
+    contenedor.addEventListener("click", function(){
         alert('Hola! Soy el div.')
     })
 })
